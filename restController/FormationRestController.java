f3e0ee88be97a1e074d3da6b1964b2de7fe2eb29
legoeland06 <45659378+legoeland06@users.noreply.github.com>
2022-08-02 11:30:48 +0200

    modifs restcontroller

--- restController/FormationRestController.java
+++ restController/FormationRestController.java
@@ -1,9 +1,11 @@
 package com.example.demo.restController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.example.demo.entities.Formation;
+import com.example.demo.services.IFormations;
 import com.example.demo.services.IService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -24,12 +27,12 @@ import org.springframework.web.server.ResponseStatusException;
 public class FormationRestController {
 
 	@Autowired
-	private IService<Formation> formationService;
+	private IFormations formationIFormation;
 	
 	@PutMapping("/formations/{id}")
 	public ResponseEntity<Formation> modify(@PathVariable Long id, @RequestBody Formation target) {
 
-		Formation fm = formationService.findById(id).orElseThrow(
+		Formation fm = formationIFormation.findById(id).orElseThrow(
 				() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Formation not found with id : " + id));
 		
 		fm.setIntitule(target.getIntitule());
@@ -37,33 +40,51 @@ public class FormationRestController {
 		fm.setPrix(target.getPrix());
 		fm.setDescriptif(target.getDescriptif());
 		
-		return new ResponseEntity<Formation>(formationService.saveOrUpdate(fm), HttpStatus.OK);
+		return new ResponseEntity<Formation>(formationIFormation.saveOrUpdate(fm), HttpStatus.OK);
 	}
 	
 	@GetMapping("/formations")
 	public ResponseEntity<List<Formation>> showAll() {
-		return new ResponseEntity<List<Formation>>(formationService.findAll(), HttpStatus.OK);
+		return new ResponseEntity<List<Formation>>(formationIFormation.findAll(), HttpStatus.OK);
 	}
 	
 	@GetMapping("/formations/{id}")
 	public Optional<Formation> findById(@PathVariable Long id) {
-		return formationService.findById(id);
+		return formationIFormation.findById(id);
 	}
 	
 	@GetMapping("/formations/findByIntitule/{intitule}")
 	public Optional<List<Formation>> findByNom(@PathVariable String intitule) {
-		return formationService.findByNomContains(intitule);
+		return formationIFormation.findByNomContains(intitule);
+	}
+	
+	@GetMapping("/formations/findByCategorieFormations/{id}")
+	public List<Formation> findByCategorieFormations(@PathVariable Long id) {
+		List<Formation> receptacle = this.showAll().getBody();
+		List<Formation> filtree = new ArrayList<Formation>();
+		
+			receptacle.forEach(r->{
+				try {
+					if ( r.getCategorie().getId() ==  id) {
+						filtree.add(r);
+					}
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			});
+	
+		return filtree;
 	}
 	
 	@PostMapping("/formations")
 	public ResponseEntity<Formation> save(@RequestBody Formation i) {
 		
-		return new ResponseEntity<Formation>(formationService.saveOrUpdate(i), HttpStatus.OK);
+		return new ResponseEntity<Formation>(formationIFormation.saveOrUpdate(i), HttpStatus.OK);
 	}
 	@DeleteMapping("/formations/{id}")
 	public ResponseEntity<Long> save(@PathVariable Long id) {
 		
-		return new ResponseEntity<Long>(formationService.deleteById(id), HttpStatus.OK);
+		return new ResponseEntity<Long>(formationIFormation.deleteById(id), HttpStatus.OK);
 	}
 
 }
